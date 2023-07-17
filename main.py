79c1f3c188254692c9383401598a27c5d37aecd7
Techion <43059245+mathicus@users.noreply.github.com>
2023-07-17 11:10:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import numpy as np
 
+def generate_random_vectors(n):
+  """Generates a list of n random vectors."""
+  vectors = []
+  for _ in range(n):
+    vector = np.random.randint(-100, 100, size=2)
+    vectors.append(vector)
+  return vectors
+
+def main():
+  """Creates the Streamlit app."""
+  n = st.number_input("Number of vectors:", 10, 100, 10)
+  vectors = generate_random_vectors(n)
+  st.title("Cartesian Graph with Random Vectors")
+  st.plot(vectors)
+
+if __name__ == "__main__":
+  main()
